0d9e5196161b64d3725aa52acc4cfc39f57d7eb3
Vladimir Rech <rechvladimir@hotmail.com>
2020-07-13 21:16:55 -0300

    More tuples samples and details

--- tuples.py
+++ tuples.py
@@ -30,3 +30,20 @@ print(dog.LegsNumber)
 print("Tuble with Multiple object types")
 animals = [1, 2, 3], 'A DOG', ('DOG LEGS', dog)
 print(animals)
+
+# Build empty tuple
+empty = ()      # with empty closed parentesis
+print(len(empty))
+
+# Tuple with a single value
+singleton = 'Just one value',      # defined by the coma at end of the line
+print(len(singleton))
+
+# It's possible to unpacking the elements of a tuple into variables
+# Just assign with the same number of variables than elements
+myTuple = (123, 456, 789)
+print(myTuple)
+x, y, z = myTuple
+print(x)
+print(y)
+print(z)
