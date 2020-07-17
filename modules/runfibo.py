9ab29e45de65480e7ca791fa680c62e02bb08a34
Vladimir Rech <rechvladimir@hotmail.com>
2020-07-16 21:01:19 -0300

    File to demonstrate import modules on Python

---
+++ modules/runfibo.py
@@ -0,0 +1,12 @@
+# runfibo.py
+# demonstrate how to use import modules on Python
+# Import the fibo.py
+import fibo
+
+fibo.fib(1000)
+print()
+
+f = fibo.fib2(2000)
+print(f)
+# print the module name
+print(__name__)
