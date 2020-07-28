1400c756b151ec8fcc7147f110aef5a6c75415d7
Vladimir Rech <rechvladimir@hotmail.com>
2020-07-27 23:04:12 -0300

    More Python functionalities

---
+++ modules/mod-import.py
@@ -0,0 +1,12 @@
+# mod-import.py
+# Following python modules comprehension
+
+import mymath
+import sys              # System library
+
+print("Listing funcions on mymath")
+print(dir(mymath))
+print()
+print("Listing funcions on sys")
+print(dir(sys))
+print()
