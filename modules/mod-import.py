a19bc76f69703363bf4883e707c641288c99b9b0
Vladimir Rech <rechvladimir@hotmail.com>
2020-07-27 23:07:08 -0300

    Adding builtins reference

--- modules/mod-import.py
+++ modules/mod-import.py
@@ -3,10 +3,14 @@
 
 import mymath
 import sys              # System library
+import builtins         # Builtins functions
 
 print("Listing funcions on mymath")
 print(dir(mymath))
 print()
 print("Listing funcions on sys")
 print(dir(sys))
+print()
+print("Builtins functions")
+print(dir(builtins))
 print()
