590a0438ffdc7e52d2911754f9cd500d58e8f297
Vladimir Rech <rechvladimir@hotmail.com>
2022-04-04 08:32:20 -0300

    Reading from list. Converting and other operations.

--- pandas/csv_one_column_dic.py
+++ pandas/csv_one_column_dic.py
@@ -16,3 +16,29 @@ counter = Counter(lst)
 
 for item in counter.keys():
 	print(item, ';', counter[item])
+
+# converter dictionary para dataframe para poder ordenar
+# é necessário realizar a conversão para usar os métodos da biblioteca pandas
+# para a criação do dataframe, precisamos criar os nomes das colunas
+df_new = pd.DataFrame(list(counter.items()), columns = ['date', 'occurrences'])
+print(df_new)
+print(type(df_new))
+
+# Ordenando pelo número de ocorrências
+print('\nOrdenando pelo número de ocorrências\n')
+print(df_new.sort_values(by='occurrences', ascending=False))
+
+# Ordenando pela data
+print('\nOrdenando pela data\n')
+print(df_new.sort_values(by='date', ascending=False))
+
+# Valor máximo
+print('\n** Resumos **')
+print('\nOcorrências:\n')
+print('\tValor máximo: ', df_new['occurrences'].max())
+print('\tValor mínimo: ', df_new['occurrences'].min())
+print('\tValor médio: ',  df_new['occurrences'].mean())
+
+print('\nData:\n')
+print('\tValor máximo: ', df_new['date'].max())
+print('\tValor mínimo: ', df_new['date'].min())
