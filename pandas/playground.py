4d97dedf1d64bde9d6799a6df01a405e39c4b60e
Vladimir Rech <rechvladimir@hotmail.com>
2024-11-07 16:39:10 -0300

    Bugfix

--- pandas/playground.py
+++ pandas/playground.py
@@ -8,6 +8,7 @@
 
 #
 # Convert time delta to string containg Year, Months and days
+# Input: timedelta value, return: string with year, months and days
 #
 from datetime import timedelta
 import pandas as pd
@@ -19,7 +20,7 @@ def delta_to_string(delta):
     elapsed_years = total_days // YEAR_DAYS
     elapsed_months = (total_days % YEAR_DAYS) // MONTH_DAYS
     elapsed_days = (total_days % YEAR_DAYS) % MONTH_DAYS 
-    str_ret = f"years: {elapsed_days}" if elapsed_years > 0 else ""
+    str_ret = f"years: {elapsed_years}" if elapsed_years > 0 else ""
     str_ret = str_ret + f", " if len(str_ret) > 0 and elapsed_months > 0 else ""
     str_ret = str_ret + f"months: {elapsed_months}" if elapsed_months > 0 else ""
     str_ret = str_ret + f", " if len(str_ret) > 0 and elapsed_days > 0 else ""
@@ -27,12 +28,15 @@ def delta_to_string(delta):
     return str_ret
 
 
-vdelta = timedelta(days=397)
+vdelta = timedelta(days=8618)
+# vdelta = timedelta(days=365)
 
 print(f"Original: {vdelta}, conversion: {delta_to_string(vdelta)}")
 
-vdelta = timedelta(days=365//2)
+# vdelta = timedelta(days=365//2)
+vdelta = timedelta(days=654)
 print(f"Original: {vdelta}, conversion: {delta_to_string(vdelta)}")
 
-vdelta = timedelta(days=15)
+vdelta = timedelta(days=4707)
+# vdelta = timedelta(days=15)
 print(f"Original: {vdelta}, conversion: {delta_to_string(vdelta)}")
